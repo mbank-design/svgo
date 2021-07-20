499664b965c6efd7bb679953e84e9272afadffa4
Tymon Żarski <t.zarski@mservices.com.pl>
2021-07-20 08:59:36 +0200

    feat: add validation utilities methods

--- pluginsValidate/validationUtilities.js
+++ pluginsValidate/validationUtilities.js
@@ -11,3 +11,54 @@ function findElementByName(svg, name) {
 }
 
 exports.findElementByName = findElementByName;
+
+function findAllElementByName(svg, name) {
+  var result = [];
+  walkTree(svg, function (node) {
+    if (node.type === 'element' && node.name === name) {
+      result.push(node);
+    }
+  });
+  return result;
+}
+exports.findAllElementByName = findAllElementByName;
+
+//find all elements with a given attribute
+function findAllElementByAttribute(svg, attribute) {
+  var result = [];
+  walkTree(svg, function (node) {
+    if (
+      node.type === 'element' &&
+      node.attributes.hasOwnProperty.call(node.attributes, attribute)
+    ) {
+      result.push(node);
+    }
+  });
+  return result;
+}
+exports.findAllElementByAttribute = findAllElementByAttribute;
+
+//find all elements with a given attribute
+function findAllElementByAttributeValue(svg, attribute, value) {
+  var result = [];
+  walkTree(svg, function (node) {
+    if (
+      node.type === 'element' &&
+      node.attributes.hasOwnProperty.call(node.attributes, attribute) &&
+      node.attributes[attribute] === value
+    ) {
+      result.push(node);
+    }
+  });
+  return result;
+}
+exports.findAllElementByAttributeValue = findAllElementByAttributeValue;
+
+//find all elements with a given attribute
+
+function walkTree(svg, callback) {
+  for (const child of svg.children) {
+    callback(child);
+    walkTree(child, callback);
+  }
+}
